55fb2862cef3705f4fe64d8e1fbd66a4a7473ab1
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-05-27 22:34:11 -0500

    added some comments to remember lazy loading

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -38,12 +38,29 @@ packer.init {
   },
 }
 
--- Install your plugins here
+----------- Install your plugins here -----------
 return packer.startup(function(use)
   -- My plugins here
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  
+  --[[ Lazy Loading
+
+  -- Load on specific commands
+  use {'my/plugin', opt = true, cmd = {'Make', 'Focus'}
+
+  
+  -- Load on a combination of conditions: specific filetypes or commands
+  use {
+    'my/plugin',
+    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+  }
+
+  -- Plugins can have post-install/update hooks
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  --]]
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins

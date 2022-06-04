26524a4162b1a5540b9f8af0663ed2e804ddc070
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-06-04 16:12:55 -0500

    format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,13 +8,29 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
+-- autocommand to format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+null_ls.setup({
   debug = false,
   sources = {
     formatting.prettier,
     formatting.black,
     -- formatting.yapf,
     formatting.stylua,
+
     diagnostics.flake8,
   },
-}
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+})

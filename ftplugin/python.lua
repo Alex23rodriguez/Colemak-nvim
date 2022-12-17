ea096016c2cad5b7657fc6df3f2e0dabd946df8f
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-12-16 18:18:20 -0600

    feat: added some keymaps for jupyter

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,5 +1,15 @@
 local opts = { noremap = true, silent = true }
 
 vim.cmd("let g:jupyter_mapkeys = 0")
+vim.api.nvim_set_keymap("n", "<leader>jj", "<cmd>JupyterConnect<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>jd", "<cmd>JupyterDisconnect<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>jf", "<cmd>JupyterRunFile<CR>", opts)
+
 vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>JupyterSendCell<cr>", opts)
 vim.api.nvim_set_keymap("v", "<leader>c", "<cmd>JupyterSendRange<cr>", opts)
+
+vim.api.nvim_set_keymap("n", "#u", "o<CR><CR>###<esc>kk", opts)
+vim.api.nvim_set_keymap("n", "#e", "i###<CR><CR><esc>", opts)
+vim.api.nvim_set_keymap("n", "#i", "i###<CR><CR><CR><CR>###<esc>kk", opts)
+vim.api.nvim_set_keymap("n", "#d", "/###<CR>dN:noh<CR>", opts)
+vim.api.nvim_set_keymap("n", "##", "i###<esc>", opts)

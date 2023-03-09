816bd9dd5ee4892003d1788d280b4e292984eb72
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-08 19:33:21 -0600

    swapped packer for lazy.nvim! (packer version at plugins.lua.bak)

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,176 +1,116 @@
--- Automatically install packer
---[[ local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim" ]]
---[[ if fn.empty(fn.glob(install_path)) > 0 then ]]
---[[   PACKER_BOOTSTRAP = fn.system({ ]]
---[[     "git", ]]
---[[     "clone", ]]
---[[     "--depth", ]]
---[[     "1", ]]
---[[     "https://github.com/wbthomason/packer.nvim", ]]
---[[     install_path, ]]
---[[   }) ]]
---[[   print("Installing packer close and reopen Neovim...") ]]
---[[   vim.cmd([[packadd packer.nvim]]
---[[ end ]]
-
 -- Automatically install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-
---[[ vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-   augroup end
-]]
-
--- Use a protected call so we don't error out on first use
---[[ local status_ok, packer = pcall(require, "packer") ]]
---[[ if not status_ok then ]]
---[[   return ]]
---[[ end ]]
 
 -- Use a protected call so we don't error out on first use
 local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-  return
+	return
 end
 
--- Have packer use a popup window
---[[ packer.init({ ]]
---[[   display = { ]]
---[[     open_fn = function() ]]
---[[       return require("packer.util").float({ border = "rounded" }) ]]
---[[     end, ]]
---[[   }, ]]
---[[ }) ]]
+--- https://github.com/folke/lazy.nvim.git ---
+--------- Install your plugins here ----------
+plugins = {
+	-- core
+	"folke/lazy.nvim", -- Have packer manage itself
+	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
 
------------ Install your plugins here -----------
---[[ return packer.startup(function(use) ]]
+	-- misc
+	"windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+	"numToStr/Comment.nvim", -- Easily comment stuff
+	"kyazdani42/nvim-web-devicons",
+	"kyazdani42/nvim-tree.lua",
+	"akinsho/bufferline.nvim",
+	"moll/vim-bbye",
+	"nvim-lualine/lualine.nvim",
+	"akinsho/toggleterm.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	"gennaro-tedesco/nvim-jqx",
+	"lewis6991/impatient.nvim",
+	"norcalli/nvim-colorizer.lua",
 
-lazy.setup({
-  -- My plugins here
-  -- core
-  -- "wbthomason/packer.nvim", -- Have packer manage itself
-  "folke/lazy.nvim",
-  "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+	-- motion
+	"jeetsukumaran/vim-indentwise",
+	{ "unblevable/quick-scope", enabled = false },
+	"tpope/vim-surround",
 
-  -- misc
-  "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
-  "numToStr/Comment.nvim", -- Easily comment stuff
-  "kyazdani42/nvim-web-devicons",
-  "kyazdani42/nvim-tree.lua",
-  "akinsho/bufferline.nvim",
-  "moll/vim-bbye",
-  "nvim-lualine/lualine.nvim",
-  "akinsho/toggleterm.nvim",
-  "lukas-reineke/indent-blankline.nvim",
-  "gennaro-tedesco/nvim-jqx",
-  "lewis6991/impatient.nvim",
-  "norcalli/nvim-colorizer.lua",
+	-- navigation
+	"ThePrimeagen/harpoon", --[[ https://github.com/ThePrimeagen/harpoon ]]
+	"goolord/alpha-nvim",
+	"ahmedkhalf/project.nvim",
+	"aserowy/tmux.nvim",
 
-  -- motion
-  "jeetsukumaran/vim-indentwise",
-  "unblevable/quick-scope",
-  "tpope/vim-surround",
+	-- cmp plugins
+	"hrsh7th/nvim-cmp", -- The completion plugin
+	"hrsh7th/cmp-buffer", -- buffer completions
+	"hrsh7th/cmp-path", -- path completions
+	"hrsh7th/cmp-cmdline", -- cmdline completions
+	"saadparwaiz1/cmp_luasnip", -- snippet completions
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-nvim-lua",
 
-  -- navigation
-  "ThePrimeagen/harpoon", --[[ https://github.com/ThePrimeagen/harpoon ]]
-  "goolord/alpha-nvim",
-  "ahmedkhalf/project.nvim",
-  "aserowy/tmux.nvim",
+	-- snippets
+	"L3MON4D3/LuaSnip", --snippet engine
+	"rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
-  -- cmp plugins
-  "hrsh7th/nvim-cmp", -- The completion plugin
-  "hrsh7th/cmp-buffer", -- buffer completions
-  "hrsh7th/cmp-path", -- path completions
-  "hrsh7th/cmp-cmdline", -- cmdline completions
-  "saadparwaiz1/cmp_luasnip", -- snippet completions
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-nvim-lua",
+	-- LSP
+	{ "neovim/nvim-lspconfig", lazy = false }, -- enable LSP
+	{ "williamboman/mason.nvim", lazy = false },
+	{ "williamboman/mason-lspconfig.nvim", lazy = false },
+	"jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
 
-  -- snippets
-  "L3MON4D3/LuaSnip", --snippet engine
-  "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+	-- Telescope
+	"nvim-telescope/telescope.nvim",
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	"nvim-telescope/telescope-media-files.nvim",
 
-  -- LSP
-  "neovim/nvim-lspconfig", -- enable LSP
-  "williamboman/nvim-lsp-installer", -- simple to use language server installer
-  "jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
+	-- Colorschemes
+	"lunarvim/colorschemes",
+	"mangeshrex/everblush.vim",
+	"folke/tokyonight.nvim",
 
-  -- Telescope
-  "nvim-telescope/telescope.nvim",
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  "nvim-telescope/telescope-media-files.nvim",
+	-- Treesitter
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"p00f/nvim-ts-rainbow",
+	"nvim-treesitter/playground",
+	"JoosepAlviste/nvim-ts-context-commentstring",
 
-  -- Colorschemes
-  "lunarvim/colorschemes",
-  "mangeshrex/everblush.vim",
-  "folke/tokyonight.nvim",
+	-- Git
+	"lewis6991/gitsigns.nvim",
+	"sindrets/diffview.nvim",
+	"ruifm/gitlinker.nvim",
 
-  -- Treesitter
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  "p00f/nvim-ts-rainbow",
-  "nvim-treesitter/playground",
-  "JoosepAlviste/nvim-ts-context-commentstring",
+	-- Web dev
+	"windwp/nvim-ts-autotag", --[[ autoclose tags in jsx and html https://github.com/windwp/nvim-ts-autotag ]]
 
-  -- Git
-  "lewis6991/gitsigns.nvim",
-  "sindrets/diffview.nvim",
-  "ruifm/gitlinker.nvim",
+	-- Python
+	"jupyter-vim/jupyter-vim",
 
-  -- Web dev
-  "windwp/nvim-ts-autotag", --[[ autoclose tags in jsx and html https://github.com/windwp/nvim-ts-autotag ]]
+	-- LaTeX
+	"lervag/vimtex",
 
-  -- Python
-  "jupyter-vim/jupyter-vim",
+	-- Debugging
+	"mfussenegger/nvim-dap",
+	"rcarriga/nvim-dap-ui",
+	"theHamsta/nvim-dap-virtual-text",
+	"mfussenegger/nvim-dap-python",
 
-  -- LaTeX
-  "lervag/vimtex",
+	-- SSH
+	-- plugin to copy across ssh
+	"ojroques/nvim-osc52",
+}
 
-  -- Debugging
-  "mfussenegger/nvim-dap",
-  "rcarriga/nvim-dap-ui",
-  "theHamsta/nvim-dap-virtual-text",
-  "mfussenegger/nvim-dap-python",
+opts = {}
 
-  -- SSH
-  -- plugin to copy across ssh
-  "ojroques/nvim-osc52",
-
-  --[[ Lazy Loading
-
-  -- Load on specific commands
-  use {'my/plugin', opt = true, cmd = {'Make', 'Focus'}
-
-
-  -- Load on a combination of conditions: specific filetypes or commands
-  use {
-    'my/plugin',
-    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-  }
-
-  -- Plugins can have post-install/update hooks
-  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-
-  --]]
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  --[[ if PACKER_BOOTSTRAP then ]]
-  --[[ require("packer").sync() ]]
-  --[[ end ]]
-})
+lazy.setup(plugins, opts)

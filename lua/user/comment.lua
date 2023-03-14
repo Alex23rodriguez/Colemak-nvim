f9d0d844e91558a414ee51a86ea687deef1538d2
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-03-14 12:26:34 -0600

    removed Comment setup because it was messing with python comments

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -1,67 +1,9 @@
 local status_ok, comment = pcall(require, "Comment")
 if not status_ok then
-  return
+	return
 end
 
-comment.setup({
+comment.setup()
 
-  ---LHS of toggle mappings in NORMAL + VISUAL mode
-  ---@type table
-  toggler = {
-    ---Line-comment toggle keymap
-    line = "gcc",
-    ---Block-comment toggle keymap
-    block = "gbc",
-  },
-
-  ---LHS of operator-pending mappings in NORMAL + VISUAL mode
-  ---@type table
-  opleader = {
-    ---Line-comment keymap
-    line = "gc",
-    ---Block-comment keymap
-    block = "gb",
-  },
-
-  ---LHS of extra mappings
-  ---@type table
-  extra = {
-    ---Add comment on the line above
-    above = "gcO",
-    ---Add comment on the line below
-    below = "gco",
-    ---Add comment at the end of line
-    eol = "gcA",
-  },
-
-  pre_hook = function(ctx)
-    local U = require("Comment.utils")
-
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
-    end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring({
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    })
-  end,
-})
-
--- Examples
---[[ `gcw` - Toggle from the current cursor position to the next word
-`gc$` - Toggle from the current cursor position to the end of line
-`gc}` - Toggle until the next blank line
-`gc5j` - Toggle 5 lines after the current cursor position
-`gc8k` - Toggle 8 lines before the current cursor position
-`gcip` - Toggle inside of paragraph
-`gca}` - Toggle around curly brackets
-
-# Blockwise
-
-`gb2}` - Toggle until the 2 next blank line
-`gbaf` - Toggle comment around a function (w/ LSP/treesitter support)
-`gbac` - Toggle comment around a class (w/ LSP/treesitter support) ]]
+-- Check out defualt config here:
+-- https://github.com/numToStr/Comment.nvim#configuration-optional

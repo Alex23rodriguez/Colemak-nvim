1cdd49422aadd0b3c3b60839843a4606408c924b
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-09-30 02:13:04 +0200

    python keymaps now only for that buffer

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,18 +1,18 @@
 local opts = { noremap = true, silent = true }
 
 vim.cmd("let g:jupyter_mapkeys = 0")
-vim.api.nvim_set_keymap("n", "<leader>jj", "<cmd>JupyterConnect<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>jd", "<cmd>JupyterDisconnect<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>jf", "<cmd>JupyterRunFile<CR>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>jj", "<cmd>JupyterConnect<CR>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>jd", "<cmd>JupyterDisconnect<CR>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>jf", "<cmd>JupyterRunFile<CR>", opts)
 
-vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>JupyterSendCell<cr>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>c", "<cmd>JupyterSendCell<cr>", opts)
 -- workaround send range in visual mode
 -- exits and enters visual mode to set '< and '>
 -- note that it also sets a mark in 'j, so may override whatever may be there
-vim.api.nvim_set_keymap("v", "<leader>c", "mjvv<cmd>'<,'>JupyterSendRange<cr>v'j", opts)
+vim.api.nvim_buf_set_keymap(0, "v", "<leader>c", "mjvv<cmd>'<,'>JupyterSendRange<cr>v'j", opts)
 
-vim.api.nvim_set_keymap("n", "#u", "o<CR><CR>###<esc>kk", opts)
-vim.api.nvim_set_keymap("n", "#e", "i###<CR><CR><esc>", opts)
-vim.api.nvim_set_keymap("n", "#i", "i###<CR><CR><CR><CR>###<esc>kk", opts)
-vim.api.nvim_set_keymap("n", "#d", "/###<CR>dN:noh<CR>", opts)
-vim.api.nvim_set_keymap("n", "##", "i###<esc>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "#u", "o<CR><CR>###<esc>kk", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "#e", "i###<CR><CR><esc>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "#i", "i###<CR><CR><CR><CR>###<esc>kk", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "#d", "/###<CR>dN:noh<CR>", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "##", "i###<esc>", opts)

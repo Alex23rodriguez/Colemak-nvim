f7da89ce97d6c5d95898ea85d9e002392862afa5
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2023-09-30 02:13:19 +0200

    added uiua keymaps

--- ftplugin/uiua.lua
+++ ftplugin/uiua.lua
@@ -3,6 +3,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*.ua",
 	callback = function()
 		vim.lsp.buf.format({ async = false })
-		return true
+		-- return true
 	end,
 })
+
+vim.cmd("setl commentstring=#%s")
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_buf_set_keymap(0, "n", "j", "d$O<esc>p", opts)
+vim.api.nvim_buf_set_keymap(0, "n", "<A-j>", "ddpkJ", opts)

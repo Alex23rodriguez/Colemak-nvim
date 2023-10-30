75b031cb8b98dfd9f1ac842ab77d3af108511708
Alejandro Rodriguez <alejandro.rodriguez@partner.bmwgroup.com>
2023-10-30 15:12:38 +0000

    easier quickscope colors for mono terminals

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,8 +47,8 @@ local plugins = {
 		lazy = false,
 		init = function()
 			vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-			vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#e37933", underline = true, ctermfg = 155 })
-			vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#e8274b", underline = true, ctermfg = 81 })
+			vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#ffff00", underline = true, ctermfg = 155 })
+			vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#ff0000", underline = true, ctermfg = 81 })
 		end,
 	},
 	"tpope/vim-surround",

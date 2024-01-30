b3bfa39d12733c9feb02febc2e8470dc8611ad97
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-01-30 18:49:20 +0100

    added neotest to plugins

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		-- add adapters here
+		"nvim-neotest/neotest-python",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python"),
+			},
+		})
+	end,
+}

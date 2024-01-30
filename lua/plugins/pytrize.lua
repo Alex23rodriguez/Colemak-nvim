b807d3518384997d5508584932851b87bbec10fc
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-01-30 19:02:41 +0100

    added keymap to pytrize

--- lua/plugins/pytrize.lua
+++ lua/plugins/pytrize.lua
@@ -1,6 +1,14 @@
 return {
 	"AckslD/nvim-pytrize.lua",
 	config = function()
-		require("pytrize").setup()
+		require("pytrize").setup({ no_commands = true })
 	end,
+	keys = {
+		{
+			"gF",
+			"<cmd>lua require('pytrize.api').jump_fixture()<cr>",
+			ft = "python",
+			desc = "Pytrize: Jump to Fixture",
+		},
+	},
 }

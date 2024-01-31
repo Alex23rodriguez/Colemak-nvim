7cea4612f76ab8f1c70c6e01f128c0d84198cda7
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-01-31 12:07:51 +0100

    move to quickscope to plugins folder

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -38,15 +38,6 @@ local plugins = {
 
 	-- motion
 	"jeetsukumaran/vim-indentwise",
-	{
-		"unblevable/quick-scope",
-		lazy = false,
-		init = function()
-			vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-			vim.api.nvim_set_hl(0, "QuickScopePrimary", { fg = "#e37933", underline = true, ctermfg = 155 })
-			vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#e8274b", underline = true, ctermfg = 81 })
-		end,
-	},
 	"tpope/vim-surround",
 
 	-- navigation

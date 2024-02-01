15d0e4cdaeed6723c33784f41da5d2b9f6db2248
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-02-01 23:04:07 +0100

    added moves to treesitter textobjects

--- lua/plugins/treesitter/treesitter_plugins.lua
+++ lua/plugins/treesitter/treesitter_plugins.lua
@@ -37,11 +37,51 @@ return {
 							["<leader>sl"] = "@function.outer",
 						},
 					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = { query = "@call.outer", desc = "Next function call start" },
+							["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+							["]c"] = { query = "@class.outer", desc = "Next class start" },
+							["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+							["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+						},
+						goto_next_end = {
+							["]F"] = { query = "@call.outer", desc = "Next function call end" },
+							["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+							["]C"] = { query = "@class.outer", desc = "Next class end" },
+							["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+							["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+						},
+						goto_previous_start = {
+							["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+							["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+							["[c"] = { query = "@class.outer", desc = "Prev class start" },
+							["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+							["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+						},
+						goto_previous_end = {
+							["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+							["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+							["[C"] = { query = "@class.outer", desc = "Prev class end" },
+							["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+							["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+						},
+					},
 				},
 				playground = {
 					enable = true,
 				},
 			})
+
+			-- local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+			-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+			-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+			-- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+			-- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+			-- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+			-- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 		end,
 	},
 	{

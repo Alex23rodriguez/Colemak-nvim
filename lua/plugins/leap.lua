548a8e22e0c86779a05bcf37a282ad746ceeec2b
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-02-01 12:43:29 +0100

    added leap plugin

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,8 @@
+return {
+	"ggandor/leap.nvim",
+	keys = {
+		{ "SI", "<Plug>(leap-forward)", mode = { "n", "x", "o" } },
+		{ "SN", "<Plug>(leap-backward)", mode = { "n", "x", "o" } },
+		{ "SE", "<Plug>(leap-from-window)", mode = { "n", "x", "o" } },
+	},
+}

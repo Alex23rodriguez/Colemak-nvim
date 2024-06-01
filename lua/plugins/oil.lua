9313381f3746686b0f633a60555770caf79265c8
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-01 17:21:45 +0200

    added oil.nvim plugin

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,54 @@
+return {
+	"stevearc/oil.nvim",
+	lazy = false,
+	keys = {
+		{ "N", "<cmd>Oil --float<CR>", desc = "Open parent directory" },
+	},
+	opts = {
+		keymaps = {
+			["<CR>"] = "actions.select",
+			["S"] = "actions.change_sort",
+			["N"] = "actions.close",
+			["R"] = "actions.refresh",
+			["P"] = "actions.preview",
+			["X"] = "actions.open_cmdline",
+			["T"] = "actions.open_terminal",
+			["Y"] = "actions.preview_scroll_down",
+			["L"] = "actions.preview_scroll_up",
+			["g?"] = "actions.show_help",
+			["gi"] = function()
+				require("oil").set_columns({
+					{ "permissions", highlight = "@attribute" },
+					{ "mtime", format = "%Y-%m-%d %H:%M", highlight = "@function" },
+					{ "size", highlight = "@keyword" },
+					"icon",
+				})
+			end,
+			["gn"] = function()
+				require("oil").set_columns({ "icon" })
+			end,
+			["gs"] = function()
+				require("oil").set_columns({ { "size", highlight = "@keyword" }, "icon" })
+			end,
+			["gm"] = function()
+				require("oil").set_columns({
+					{ "mtime", format = "mod: %Y-%m-%d %H:%M", highlight = "@function" },
+					"icon",
+				})
+			end,
+			["gc"] = function()
+				require("oil").set_columns({
+					{ "ctime", format = "created: %Y-%m-%d %H:%M", highlight = "@function" },
+					"icon",
+				})
+			end,
+			["ga"] = function()
+				require("oil").set_columns({
+					{ "atime", format = "accessed: %Y-%m-%d %H:%M", highlight = "@function" },
+					"icon",
+				})
+			end,
+		},
+		view_options = { show_hidden = true },
+	},
+}

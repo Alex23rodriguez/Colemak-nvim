01dd365dc2733e79bbd47db255b3c24e97538fc5
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-06-01 17:32:58 +0200

    fixed insort

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -33,6 +33,6 @@ null_ls.setup({
 vim.api.nvim_create_user_command("Format", function()
 	vim.lsp.buf.format({ async = false })
 	if vim.filetype.match({ buf = vim.api.nvim_get_current_buf() }) == "python" then
-		vim.cmd("!ruff --fix --select I %")
+		vim.cmd("!ruff check --fix --select I %")
 	end
 end, { desc = "format the document and sort imports" })
